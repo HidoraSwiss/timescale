4a47ae8d4387175526f509b227e59b91f22635be
Dubouchj <33114629+Dubouchj@users.noreply.github.com>
2019-01-03 16:40:16 +0100

    Update replication.sh

--- replication.sh
+++ replication.sh
@@ -1,44 +1,5 @@
 #!/bin/bash
 
-# CONFIGURE PRIMARY
-if [[ -z $REPLICATE_FROM ]]; then
-
-psql -U postgres -c "SET password_encryption = 'scram-sha-256'; CREATE ROLE $REPLICA_POSTGRES_USER WITH REPLICATION PASSWORD '$REPLICA_POSTGRES_PASSWORD' LOGIN;"
-
-# Add replication settings to primary postgres conf
-cat >> ${PGDATA}/postgresql.conf <<EOF
-listen_addresses= '*'
-wal_level = replica
-max_wal_senders = 2
-max_replication_slots = 2
-synchronous_commit = ${SYNCHRONOUS_COMMIT}
-EOF
-
-# Add synchronous standby names if we're in one of the synchronous commit modes
-if [[ "${SYNCHRONOUS_COMMIT}" =~ ^(on|remote_write|remote_apply)$ ]]; then
-cat >> ${PGDATA}/postgresql.conf <<EOF
-synchronous_standby_names = '1 (${REPLICA_NAME})'
-EOF
-fi
-
-# Add replication settings to primary pg_hba.conf
-# Using the hostname of the primary doesn't work with docker containers, so we resolve to an IP using getent,
-# or we use a subnet provided at runtime.
-if  [[ -z $REPLICATION_SUBNET ]]; then
-    REPLICATION_SUBNET=$(getent hosts ${REPLICATE_TO} | awk '{ print $1 }')/32
-fi
-
-cat >> ${PGDATA}/pg_hba.conf <<EOF
-host     replication     ${REPLICA_POSTGRES_USER}   ${REPLICATION_SUBNET}       scram-sha-256
-EOF
-
-# Restart postgres and add replication slot
-pg_ctl -D ${PGDATA} -m fast -w restart
-psql -U postgres -c "SELECT * FROM pg_create_physical_replication_slot('${REPLICA_NAME}_slot');"
-
-# CONFIGURE REPLICA
-else
-
 # Stop postgres instance and clear out PGDATA
 sudo -u postgres -i bash -c 'pg_ctl -D ${PGDATA} -w stop'
 pg_ctl -D ${PGDATA} -m fast -w stop
@@ -75,4 +36,4 @@ chown postgres:postgres ${PGDATA}/recovery.conf
 chmod 0600 ${PGDATA}/recovery.conf
 
 sudo -u postgres -i bash -c 'pg_ctl -D ${PGDATA} -w start'
-fi
+
