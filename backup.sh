702a1a97f0c0c7919e6fe45caf7b55371833f7d5
Dubouchj <33114629+Dubouchj@users.noreply.github.com>
2019-01-03 11:58:11 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,8 @@
+# Backup replica from the primary
+until PGPASSFILE=~/.pgpass.conf pg_basebackup -h ${REPLICATE_FROM} -D ${PGDATA} -U ${POSTGRES_USER} -vP -w
+do
+    # If docker is starting the containers simultaneously, the backup may encounter
+    # the primary amidst a restart. Retry until we can make contact.
+    sleep 1
+    echo "Retrying backup . . ."
+done
