f0172572eabd7abd94f9d6e11a6eae52bb435a73
Dubouchj <33114629+Dubouchj@users.noreply.github.com>
2019-01-03 09:55:09 +0100

    Update replication.sh

--- replication.sh
+++ replication.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CONFIGURE PRIMARY
-if [[ -z $AAAA ]]; then
+if [[ -z $REPLICATE_FROM ]]; then
 
 psql -U postgres -c "SET password_encryption = 'scram-sha-256'; CREATE ROLE $REPLICA_POSTGRES_USER WITH REPLICATION PASSWORD '$REPLICA_POSTGRES_PASSWORD' LOGIN;"
 
